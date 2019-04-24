97a1e4192193e4abffd67a27513b18cea43982aa
Foxydoxy <34740761+Foxydoxy@users.noreply.github.com>
2019-04-24 19:15:44 +0100

    some changes

--- codewriting.jsx
+++ codewriting.jsx
@@ -9,27 +9,25 @@ function getCode(filepath){
   return contents;
 }
 
-function getRanges(codeStr){
-
-}
-
-function getExpression(){
+function getExpression(points){
   var expr = "i = textIndex;\n"
-  +"if(\n"
-  + getRanges();
-  +"0){100}else{0}";
+  +"if(\n";
+  for(var i=0;i<points.length;i++){
+    expr += "("+points[i][0]+" <= i && i <= "+points[i][1]+") ||\n";
+  }
+  expr += "0){100}else{0}";
   return expr;
 }
 
-function addAnimatorProp(textLayer,animatorName,thingToHighlight,highLightingColor){
+function addAnimatorProp(textLayer,animatorName,animExpression,highLightingColor){
   var txtAnims = textLayer.property("ADBE Text Properties").property(4);
   var txtAnimator = txtAnims.addProperty("ADBE Text Animator");
   txtAnimator.name = animatorName;
 
   // Modify the amount expression:
   var expressionSelector = txtAnimator.property(1).addProperty("ADBE Text Expressible Selector");// Add an expression selector
-  expressionSelector.property("Based On").setValue(3); // set to words
-  expressionSelector.property("Amount").expression = getAnimatorExpression(thingToHighlight,false); // get expression
+  expressionSelector.property("Based On").setValue(1); // set to chars
+  expressionSelector.property("Amount").expression = animExpression; // get expression
 
   // Add the appropriate fill color:
   var colorSelector = txtAnimator.property("ADBE Text Animator Properties").addProperty("ADBE Text Fill Color");
@@ -45,40 +43,19 @@ function getSyntaxJSON(){
   return jsonObj;
 }
 
-function codeTextLayer(codeStr){
-  var comp = app.project.activeItem;
-  var text = comp.layers.addText(codeStr);
-  var jsonObj = getSyntaxJSON();
-  for(var i=0;i<jsonObj.length;i++){
-    var name = jsonObj[i].name;
-    var thing = jsonObj[i].thing;
-    var color = jsonObj[i].color;
-    addAnimatorProp(text,name,thing,color);
-  }
-  return text;
-}
-
-function getSyntaxJSON(){
-  var jsonFile = File("syntax.json");
-  jsonFile.open('r');
-  jsonContent = jsonFile.read();
-  jsonFile.close();
-  var jsonObj = JSON.parse(jsonContent);
-  return jsonObj;
-}
-
 function getPoints(text,patt,replacepatt){
     var points = [];
     var counter = 0;
     var tempStr = "";
-    while(match = patt.exec(text) != null){
+    var str1 = "";
+    while((match = patt.exec(text)) != null){
       fi = match.index;
       li = patt.lastIndex;
       tempStr = text.substring(fi,li).replace(replacepatt,"");
       points[counter] = [];
       points[counter][0] = fi;
       points[counter][1] = fi + tempStr.length;
-      counter++
+      counter++;
     }
     return points;
 }
@@ -86,21 +63,31 @@ function getPoints(text,patt,replacepatt){
 function testPoints(text,p){
   var str1 = "";
   for(var i=0;i<p.length;i++){
-    str1 = text.substring(points[i][0],points[i][1]);
+    str1 = text.substring(p[i][0],p[i][1]);
     alert(str1);
   }
 }
 
-var text = py();
-var j = getSyntaxJSON();
-for(var i= 0;i<){
-  var pattern = eval(j[0].pattern);
-  var replacepattern = eval(j[0].replacepattern);
-
-  var p = getPoints(text,functionPatt,replacepatt);
+function codeTextLayer(codeStr){
+  var comp = app.project.activeItem;
+  var text = comp.layers.addText(codeStr);
+  var jsonObj = getSyntaxJSON();
+  for(var i=0;i<jsonObj.length;i++){
+    var name = jsonObj[i].name;
+    var pattern = eval(jsonObj[i].pattern);
+    var replacepattern = eval(jsonObj[i].replacepattern);
+    var color = jsonObj[i].color;
+    var points = getPoints(codeStr,pattern,replacepattern);
+    //testPoints(codeStr,points);
+    alert("layer test");
+    var ttt = text.property("Text").property("Source Text").value.toString();
+    alert(typeof(ttt));
+    testPoints(ttt,points);
+    var expression = getExpression(points);
+    addAnimatorProp(text,name,expression,color);
+  }
+  return text;
 }
 
-
-
-
-//var t = codeTextLayer("hello() this is function2() for import as in return me this");
+var c = getCode("pyfile.py");
+var t = codeTextLayer(c);
