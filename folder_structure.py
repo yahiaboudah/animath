a67fe247e78d912ef3fc15b1516600b2326ed5cf
Foxydoxy <34740761+Foxydoxy@users.noreply.github.com>
2019-05-06 23:56:10 +0100

    folder_structure

---
+++ folder_structure.py
@@ -0,0 +1,6 @@
+import os
+
+PATH = 'C:/Users/HP/Desktop/Banana61/'
+
+def create_folder():
+    pass
---
+++ setMovieProxy.jsx
@@ -0,0 +1,20 @@
+var comp = app.project.activeItem;
+
+var selLayer = comp.selectedLayers[0];
+
+if(!(selLayer.source instanceof CompItem)){
+
+}
+
+function getSelLayer(){
+  
+}
+
+function createProxy(type){
+  if(type == "compLayer"){
+    selLayer = getSelLayer();
+  }
+}
+function setProxy(){
+
+}
